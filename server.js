5fc73baa98f9cf4ae7d8dbedbfd158e50fe5df9c
mobiTechindia <122245888+MobiTechindia@users.noreply.github.com>
2023-10-06 15:49:14 +0530

    update

--- server.js
+++ server.js
@@ -39,15 +39,35 @@ server.use(bodyParser.json());
 server.post('/api/process-fpx', (req, res) => {
   // Assuming the JSON data is sent in the request body
   const requestData = req.body;
+  const txnId = req.body["fpx_fpxTxnId"]
+  const sellerId = req.body["fpx_sellerId"]
+
+  let service = req.body["service"]
+
+  console.log(service);
+  
+  let response = {};
+
+  if (txnId && txnId !== "" && txnId !== "0") {
 
   // Here you can perform any processing required and return a success response
   // For now, we'll simply return a success message
-  const response = {
+   response = {
     success: true,
     message: 'Notification received successfully!',
+    txnId: txnId,
+    sellerId: sellerId
   };
-
   res.status(200).json(response);
+} else {
+  response = {
+    success: false,
+    message: 'Notification failed! Transaction ID is invalid',
+  };
+  res.status(400).json(response)
+}
+
+  
 });
 
 
