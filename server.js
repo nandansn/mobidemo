bcd79b2fc05c710d32a6b9c53655d7c702a73cb1
mobiTechindia <122245888+MobiTechindia@users.noreply.github.com>
2023-10-12 15:07:07 +0530

    update

--- server.js
+++ server.js
@@ -35,68 +35,88 @@ startServer();
 // Use middleware to parse JSON requests
 server.use(bodyParser.json());
 
+// const axios = require('axios');
 
-server.post('/api/process-fpx', (req, res) => {
+// // Replace 'example.com' with the URL you want to call.
+// const url = 'http://example.com';
+
+// axios.get(url)
+//   .then((response) => {
+//     console.log(response.data);
+//   })
+//   .catch((error) => {
+//     console.error(`Error: ${error.message}`);
+//   });
+
+
+
+// server.post('/api/process-fpx', (req, res) => {
   
-  const hostDetails = {
-    host: req.headers.host,
-    origin: req.headers.origin,
-    referer: req.headers.referer,
-    userAgent: req.headers['user-agent'],
-  };  
-  console.log(hostDetails);
-  const txnId = req.body["fpx_fpxTxnId"]
-  const sellerId = req.body["fpx_sellerId"]
+//   const hostDetails = {
+//     host: req.headers.host,
+//     origin: req.headers.origin,
+//     referer: req.headers.referer,
+//     userAgent: req.headers['user-agent'],
+//   };  
+//   console.log(hostDetails);
+//   const txnId = req.body["fpx_fpxTxnId"]
+//   const sellerId = req.body["fpx_sellerId"]
 
-  let service = req.body["service"]
+//   let service = req.body["service"]
 
-  console.log(service);
+//   console.log(service);
   
-  let response = {hostDetails: hostDetails};
+//   let response = {hostDetails: hostDetails};
 
-  if (txnId && txnId !== "" && txnId !== "0") {
-
-   
-
+//   if (txnId && txnId !== "" && txnId !== "0") {
 
   
-  // condtion to test delayed response
-  if (txnId === "1234") {
-    setTimeout(() => {
-       response.data = {
-        success: true,
-        message: 'Notification received successfully after a 40-second delay!',
-        txnId: txnId,
-        sellerId: sellerId
-      };
-      res.status(200).json(response);
-    }, 20000);
-  } else if(txnId === "-1") {
-    response.data ="No Response"
-    console.log(response)
-  } else {
-    // success response immediate
-    response.data = {
-      success: true,
-      message: 'Notification received successfully!',
-      txnId: txnId,
-      sellerId: sellerId
-    };
-    res.status(200).json(response);
-  }
+  
+//   // condtion to test delayed response
+//   if (txnId === "1234") {
+//     setTimeout(() => {
+//        response.data = {
+//         success: true,
+//         message: 'Notification received successfully after a 40-second delay!',
+//         txnId: txnId,
+//         sellerId: sellerId
+//       };
+//       res.status(200).json(response);
+//     }, 20000);
+//   } else if(txnId === "-1") {
+//     response.data ="No Response"
+//     console.log(response)
+//   } else {
+//     // success response immediate
+//     response.data = {
+//       success: true,
+//       message: 'Notification received successfully!',
+//       txnId: txnId,
+//       sellerId: sellerId
+//     };
+//     res.status(200).json(response);
+//   }
 
  
    
-} else {
-  // failure response for txn id 0, empty and not passing
-  response.data = {
-    success: false,
-    message: 'Notification failed! Transaction ID is invalid',
-  };
-  res.status(400).json(response)
-}
+// } else {
+//   // failure response for txn id 0, empty and not passing
+//   response.data = {
+//     success: false,
+//     message: 'Notification failed! Transaction ID is invalid',
+//   };
+//   res.status(400).json(response)
+// }
 
   
-});
+// });
 
+
+server.post("/api/process-fpx", (req, res) => {
+
+  res.status(200).json({
+    status: "OK",
+  })
+
+ })
 
