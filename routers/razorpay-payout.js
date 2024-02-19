bbccef024793c45fc299a771d59aa1aff03e828f
nandansn <geeknanda13@gmail.com>
2024-02-19 22:04:19 +0530

    updated

--- routers/razorpay-payout.js
+++ routers/razorpay-payout.js
@@ -69,10 +69,38 @@ razorpayRouter.post('/payoutInProgress', function (req, res) {
   console.log(bankName);
 
   res.status(201).json(
-    { "Status": ["201"], "Response": [{ "batch_collection_status": ["TRANSFER_IN_PROGRESS"], "batch_id": ["BULKPAY_12345620022024001"], "batch_collection_date": ["2024-02-20 00:00:00.0"], "batch_collection_status_code": ["11"], "list": [[{ "payout_details": [null], "customer_uid": [null], "response_batch": [null], "payout_status": ["TRANSFER_IN_PROGRESS"], "payout_date": ["2024-02-20 00:00:00.0"], "payout_status_code": ["10"], "response_date": [null], "payee_name": ["WAN MOHAMAD TAJERI BIN WAN IBRAHIM-PHBMMYKL-3111020165178"], "recipient_reference": ["WAN MOHAMAD TAJERI BIN WAN IBRAHIM-PHBMMYKL-3111020165178"], "payout_amount": ["10.00"], "internal_reference": [null] }]] }], "Total": [1], "Date": ["Mon Feb 19 23:51:18 MYT 2024"] }
+    {'Status': ['201'], 'Response': [{'duitnow_payout_batch_date': ['Tue Feb 20 00:00:00 MYT 2024'], 'duitnow_payout_batch_status_code': ['11'], 'duitnow_payout_batch': ['BULKPAY_12345620022024008'], 'duitnow_payout_batch_status': ['TRANSFER_IN_PROGRESS']}], 'Date': ['Mon Feb 19 23:59:44 MYT 2024']}
   )
 
 });
+
+
+razorpayRouter.post('/payoutSuccess', function (req, res) {
+
+
+  let { bankName, subMID, amount, customerName, payoutid } = req.body;
+
+  console.log(bankName);
+
+  res.status(201).json(
+    {'Status': ['201'], 'Response': [{'duitnow_payout_batch_date': ['Tue Feb 20 00:00:00 MYT 2024'], 'duitnow_payout_batch_status_code': ['10'], 'duitnow_payout_batch': ['BULKPAY_12345620022024008'], 'duitnow_payout_batch_status': ['PROCESSED_ALL_SUCCESSFUL']}], 'Date': ['Mon Feb 19 23:59:44 MYT 2024']}
+  )
+
+});
+
+razorpayRouter.post('/payoutFailed', function (req, res) {
+
+
+  let { bankName, subMID, amount, customerName, payoutid } = req.body;
+
+  console.log(bankName);
+
+  res.status(201).json(
+    {'Status': ['201'], 'Response': [{'duitnow_payout_batch_date': ['Tue Feb 20 00:00:00 MYT 2024'], 'duitnow_payout_batch_status_code': ['9'], 'duitnow_payout_batch': ['BULKPAY_12345620022024008'], 'duitnow_payout_batch_status': ['REJECTED']}], 'Date': ['Mon Feb 19 23:59:44 MYT 2024']}
+  )
+
+});
+
 
 
 module.exports = { razorpayRouter };
