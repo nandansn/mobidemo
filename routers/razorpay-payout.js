6bb9fad82fe01fb53fe8c7d6d4e97d6b911cc74d
nandansn <geeknanda13@gmail.com>
2024-02-20 12:34:29 +0530

    updated

--- routers/razorpay-payout.js
+++ routers/razorpay-payout.js
@@ -69,7 +69,7 @@ razorpayRouter.post('/payoutInProgress', function (req, res) {
   console.log(bankName);
 
   res.status(200).json(
-    {'Status': ['201'], 'Response': [{'duitnow_payout_batch_date': ['Tue Feb 20 00:00:00 MYT 2024'], 'duitnow_payout_batch_status_code': ['11'], 'duitnow_payout_batch': ['BULKPAY_12345620022024008'], 'duitnow_payout_batch_status': ['TRANSFER_IN_PROGRESS']}], 'Date': ['Mon Feb 19 23:59:44 MYT 2024']}
+    { 'Status': ['201'], 'Response': [{ 'duitnow_payout_batch_date': ['Tue Feb 20 00:00:00 MYT 2024'], 'duitnow_payout_batch_status_code': ['11'], 'duitnow_payout_batch': ['BULKPAY_12345620022024008'], 'duitnow_payout_batch_status': ['TRANSFER_IN_PROGRESS'] }], 'Date': ['Mon Feb 19 23:59:44 MYT 2024'] }
   )
 
 });
@@ -83,7 +83,7 @@ razorpayRouter.post('/payoutSuccess', function (req, res) {
   console.log(bankName);
 
   res.status(200).json(
-    {'Status': ['201'], 'Response': [{'duitnow_payout_batch_date': ['Tue Feb 20 00:00:00 MYT 2024'], 'duitnow_payout_batch_status_code': ['10'], 'duitnow_payout_batch': ['BULKPAY_12345620022024008'], 'duitnow_payout_batch_status': ['PROCESSED_ALL_SUCCESSFUL']}], 'Date': ['Mon Feb 19 23:59:44 MYT 2024']}
+    { 'Status': ['201'], 'Response': [{ 'duitnow_payout_batch_date': ['Tue Feb 20 00:00:00 MYT 2024'], 'duitnow_payout_batch_status_code': ['10'], 'duitnow_payout_batch': ['BULKPAY_12345620022024008'], 'duitnow_payout_batch_status': ['PROCESSED_ALL_SUCCESSFUL'] }], 'Date': ['Mon Feb 19 23:59:44 MYT 2024'] }
   )
 
 });
@@ -96,7 +96,85 @@ razorpayRouter.post('/payoutFailed', function (req, res) {
   console.log(bankName);
 
   res.status(200).json(
-    {'Status': ['201'], 'Response': [{'duitnow_payout_batch_date': ['Tue Feb 20 00:00:00 MYT 2024'], 'duitnow_payout_batch_status_code': ['9'], 'duitnow_payout_batch': ['BULKPAY_12345620022024008'], 'duitnow_payout_batch_status': ['REJECTED']}], 'Date': ['Mon Feb 19 23:59:44 MYT 2024']}
+    { 'Status': ['201'], 'Response': [{ 'duitnow_payout_batch_date': ['Tue Feb 20 00:00:00 MYT 2024'], 'duitnow_payout_batch_status_code': ['9'], 'duitnow_payout_batch': ['BULKPAY_12345620022024008'], 'duitnow_payout_batch_status': ['REJECTED'] }], 'Date': ['Mon Feb 19 23:59:44 MYT 2024'] }
+  )
+
+});
+
+razorpayRouter.post('/payoutStatus/Success', function (req, res) {
+
+
+  let { bankName, subMID, amount, customerName, payoutid } = req.body;
+
+  console.log(bankName);
+
+  res.status(200).json(
+    {
+      "Status": [
+        "201"
+      ],
+      "Response": [
+        {
+          "batch_collection_status": [
+            "PROCESSED_ALL_SUCCESSFUL"
+          ],
+          "batch_id": [
+            "BULKPAY_08674926122023002"
+          ],
+          "batch_collection_date": [
+            "2023-12-26 00:00:00.0"
+          ],
+          "batch_collection_status_code": [
+            "10"
+          ],
+          "list": [
+            [
+              {
+                "payout_details": [
+                  "OCBC Test 2"
+                ],
+                "customer_uid": [
+                  null
+                ],
+                "response_batch": [
+                  null
+                ],
+                "payout_status": [
+                  "SUCCESSFULLY_COMPLETE"
+                ],
+                "payout_date": [
+                  "2023-12-26 00:00:00.0"
+                ],
+                "payout_status_code": [
+                  "0"
+                ],
+                "response_date": [
+                  null
+                ],
+                "payee_name": [
+                  "Bradley"
+                ],
+                "recipient_reference": [
+                  "Test Curlec"
+                ],
+                "payout_amount": [
+                  "1.00"
+                ],
+                "internal_reference": [
+                  null
+                ]
+              }
+            ]
+          ]
+        }
+      ],
+      "Total": [
+        1
+      ],
+      "Date": [
+        "Mon Feb 19 16:16:13 MYT 2024"
+      ]
+    }
   )
 
 });
